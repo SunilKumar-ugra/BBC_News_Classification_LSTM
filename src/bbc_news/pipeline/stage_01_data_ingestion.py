0eebf5fa8278e585a763fe216e9566499e0946dd
SunilKumar-ugra <ugargolsunilkumar@gmail.com>
2023-12-21 20:20:42 +0530

    DATA INGESTION

---
+++ src/bbc_news/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,25 @@
+from bbc_news import logger
+from bbc_news.config import ConfigurationManager
+from bbc_news.components.data_ingestion import DataIngestion
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
+
+STAGE_NAME = "Data Ingestion stage"
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
