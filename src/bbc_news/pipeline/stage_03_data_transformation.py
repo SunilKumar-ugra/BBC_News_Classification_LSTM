92766c63fa44b531fa89b6601a0e273780757b8f
SunilKumar-ugra <ugargolsunilkumar@gmail.com>
2024-01-09 16:08:13 +0530

    data transformer

--- src/bbc_news/pipeline/stage_03_data_transformation.py
+++ src/bbc_news/pipeline/stage_03_data_transformation.py
@@ -1,6 +1,6 @@
 from bbc_news.config import ConfigurationManager
 from bbc_news.components.data_transformation import DataTransformation
-
+from bbc_news import logger
 
 class DataTransformationTrainingPipeline:
     def __init__(self):
@@ -11,3 +11,14 @@ class DataTransformationTrainingPipeline:
         data_transformation_config = config.get_data_transformation_config()
         data_transformation = DataTransformation(config=data_transformation_config)
         data_transformation.transformation()
+
+STAGE_NAME = "Data Transformation stage"
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+        data_transformation = DataTransformationTrainingPipeline()
+        data_transformation.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
