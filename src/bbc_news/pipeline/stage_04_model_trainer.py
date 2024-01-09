c822fc25c23325a46bc355f0427d4bd982b5f4f6
SunilKumar-ugra <ugargolsunilkumar@gmail.com>
2024-01-09 16:07:42 +0530

    Model trainer

--- src/bbc_news/pipeline/stage_04_model_trainer.py
+++ src/bbc_news/pipeline/stage_04_model_trainer.py
@@ -1,6 +1,6 @@
 from bbc_news.config import ConfigurationManager
 from bbc_news.components.model_trainer import ModelTrainer
-
+from bbc_news import logger
 class ModelTrainingPipeline:
     def __init__(self):
         pass
@@ -12,3 +12,15 @@ class ModelTrainingPipeline:
         training.get_base_model()
         #training.train_valid_generator()
         training.train()
+        
+STAGE_NAME = "Model Trainer stage"
+if __name__ =='__main__':
+    try: 
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        model_trainer = ModelTrainingPipeline()
+        model_trainer.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
