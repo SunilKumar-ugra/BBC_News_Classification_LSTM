5b1d378f6ea1b7bd06628f12c80679b0048ddeee
Sunil Kumar <ugargolsunilkumar@gmail.com>
2025-07-25 18:08:44 +0530

    Update __init__.py

--- src/bbc_news/__init__.py
+++ src/bbc_news/__init__.py
@@ -10,7 +10,7 @@ os.makedirs(logs_path,exist_ok=True)
 LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
 logging.basicConfig(
-    format="[%(asctime)s] - %(name)s - %(levelname)s - %(module)s file - LineNum:%(lineno)d   - %(message)s",
+    format="[%(asctime)s] - %(levelname)s \nFile    : [%(module)s] File \nLineNum : %(lineno)d - %(name)s \nMessage : %(message)s \n-----------------------",
     level=logging.INFO,
     handlers=[
         logging.FileHandler(LOG_FILE_PATH),
